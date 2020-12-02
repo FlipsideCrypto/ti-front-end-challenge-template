49e86ea565bd25e3c88990a0ce4d90be8e9587e7
Jim Myers <jfmyers01@gmail.com>
2020-12-02 16:18:25 -0500

    Update FCAS API function.

--- src/api/fcas.ts
+++ src/api/fcas.ts
@@ -20,7 +20,10 @@ export type RankMetric = {
     unix_time: number
 }
 //
-// ## POST - Project Rank Metrics
+// ## POST - Project Rank Metrics, for a specified metric ('dev', 'fcas', 'market-maturity', 'utility')
+// get the current score for every project Flipside ranks.
+//
+// Rest API Docs: https://api-docs-v2.flipsidecrypto.com/#ab520b70-8ffb-46bf-b5d7-749ff0862dc5
 //
 export function getProjectRankMetrics(metric: string, change_over_in_days: number = 7): AxiosPromise<APIResponse<RankMetric[]>> {
   return client.post(`/metrics/rank/projects?api_key=${API_KEY}`, {
